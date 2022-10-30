42457cd62ff6ff13e35c242889b82557c2144406
Tia Theisen <tiatheisen@gmail.com>
2022-10-30 11:40:58 -0500

    Initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+
 
 function App() {
+
+    const dispatch = useDispatch();
+
+    //On page load, fetchFeedback
+    useEffect(() =>{
+      fetchFeedback();
+    }, [])
+    
+
+    //Axios GET request for feedback
+    const fetchFeedback = () => {
+        console.log('in FetchFeedback function');
+        axios({
+          method: 'GET', 
+          url: '/review'
+        })
+        .then((response) => {
+          dispatch ({
+            type: 'DISPLAY_FEEDBACK',
+            payload: response.data
+          })
+        })
+          .catch((err) => {
+            console.log ('error in GET display feedback', err)
+        })
+
+    }
 
   return (
     <div className='App'>
