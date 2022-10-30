c75766b69e4d91c40d51d824006101b6c08caa31
Tia Theisen <tiatheisen@gmail.com>
2022-10-30 11:56:34 -0500

    wrap return in router in App.jsx and create store instance in index.js

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -55,12 +55,14 @@ function App() {
 
 
   return (
+    <Router>
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
     </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+const storeInstance = createStore(
+    combineReducers({
+        feeling,
+        understanding,
+        support,
+        comment
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store = {storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
